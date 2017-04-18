b237e52d73c9ce8c76d0130ad0a4ff75c4628035
Andres Espinoza Bringas <aespinoza@banmedica.cl>
2017-04-18 17:38:07 -0300

    actualizacion pantalla de beacons

---
+++ html/js/beacons.js
@@ -0,0 +1,26 @@
+    /*jslint browser: true*/
+ /*global $, jQuery, alert*/
+
+   $(document).ready(function(){
+
+
+  $("#opener").on("click",function(){
+    $("#beacons").modal("show");
+  })
+
+/*    $( "Beacons" ).dialog({
+      autoOpen: false, show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "explode",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener" ).on( "click", function() {
+      $( "#dialog" ).dialog( "open" );
+    });*/
+
+});
